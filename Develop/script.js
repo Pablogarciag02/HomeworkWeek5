8e99bed5313adc44a984efd73e6cd2b4d16003ce
Pablo Garcia <pgarciag566@gmail.com>
2022-04-06 18:18:45 -0500

    added readme file, added comments, fixed js code so that each text box appears empty instead of showing null when empty.

--- Develop/script.js
+++ Develop/script.js
@@ -17,155 +17,245 @@ setInterval(() => {
 document.getElementById("currentDay").innerHTML = "Date: " + date;
 
 
-const t  = document.getElementsByTagName("textarea");
 
+//9-10am assigns color depending on time of day: There are 3 avilable options.
 var today = new Date().getHours();
 if (today >= 9 && today <= 9.59) {
-    document.getElementById("9am").style.background = "#5A7BB5";
+    document.getElementById("9am").style.background = "#BBF0D6";
 } else if (today >= 9.59 && today <= 17) {
-    document.getElementById("9am").style.background = "#B2BFC2";
-} else document.getElementById("9am").style.background = "#CCDCF2";
+    document.getElementById("9am").style.background = "#D0F9E5";
+} else document.getElementById("9am").style.background = "#BBF0D6";
 
+//10-11am assigns color depending on time of day: There are 3 avilable options.
 var today = new Date().getHours();
 if (today >= 10 && today <= 10.59) {
-    document.getElementById("10am").style.background = "#5A7BB5";
+    document.getElementById("10am").style.background = "#BBF0D6";
 } else if (today >= 10.59 && today <= 17) {
-    document.getElementById("10am").style.background = "#B2BFC2";
-} else document.getElementById("10am").style.background = "#CCDCF2";
+    document.getElementById("10am").style.background = "#D0F9E5";
+} else document.getElementById("10am").style.background = "#BBF0D6";
 
+//11-12am assigns color depending on time of day: There are 3 avilable options.
 var today = new Date().getHours();
 if (today >= 11 && today <= 11.59) {
-    document.getElementById("11am").style.background = "#5A7BB5";
+    document.getElementById("11am").style.background = "##E2EEC2";
 } else if (today >= 11.59 && today <= 17) {
-    document.getElementById("11am").style.background = "#B2BFC2";
-} else document.getElementById("11am").style.background = "#CCDCF2";
+    document.getElementById("11am").style.background = "#D0F9E5";
+} else document.getElementById("11am").style.background = "#BBF0D6";
 
+//12-13pm assigns color depending on time of day: There are 3 avilable options.
 var today = new Date().getHours();
 if (today >= 12 && today <= 12.59) {
-    document.getElementById("12am").style.background = "#5A7BB5";
+    document.getElementById("12am").style.background = "##E2EEC2";
 } else if (today >= 12.59 && today <= 17) {
-    document.getElementById("12am").style.background = "#B2BFC2";
-} else document.getElementById("12am").style.background = "#CCDCF2";
+    document.getElementById("12am").style.background = "#D0F9E5";
+} else document.getElementById("12am").style.background = "#BBF0D6";
 
+//13-14pm assigns color depending on time of day: There are 3 avilable options.
 var today = new Date().getHours();
 if (today >= 13 && today <= 13.59) {
-    document.getElementById("13pm").style.background = "#5A7BB5";
+    document.getElementById("13pm").style.background = "##E2EEC2";
 } else if (today >= 13.59 && today <= 17) {
-    document.getElementById("13pm").style.background = "#B2BFC2";
-} else document.getElementById("13pm").style.background = "#CCDCF2";
+    document.getElementById("13pm").style.background = "#D0F9E5";
+} else document.getElementById("13pm").style.background = "#BBF0D6";
 
+//14-15pm assigns color depending on time of day: There are 3 avilable options.
 var today = new Date().getHours();
 if (today >= 14 && today <= 14.59) {
-    document.getElementById("14pm").style.background = "#5A7BB5";
+    document.getElementById("14pm").style.background = "##E2EEC2";
 } else if (today >= 14.59 && today <= 17) {
-    document.getElementById("14pm").style.background = "#B2BFC2";
-} else document.getElementById("14pm").style.background = "#CCDCF2";
+    document.getElementById("14pm").style.background = "#D0F9E5";
+} else document.getElementById("14pm").style.background = "#BBF0D6";
 
+//15-16pm assigns color depending on time of day: There are 3 avilable options.
 var today = new Date().getHours();
 if (today >= 15 && today <= 15.59) {
-    document.getElementById("15pm").style.background = "#5A7BB5";
+    document.getElementById("15pm").style.background = "##E2EEC2";
 } else if (today >= 15.59 && today <= 17) {
-    document.getElementById("15pm").style.background = "#B2BFC2";
-} else document.getElementById("15pm").style.background = "#CCDCF2";
+    document.getElementById("15pm").style.background = "#D0F9E5";
+} else document.getElementById("15pm").style.background = "#BBF0D6";
 
+//16-17pm assigns color depending on time of day: There are 3 avilable options.
 var today = new Date().getHours();
 if (today >= 16 && today <= 16.59) {
-    document.getElementById("16pm").style.background = "#5A7BB5";
+    document.getElementById("16pm").style.background = "##E2EEC2";
 } else if (today >= 16.59 && today <= 17) {
-    document.getElementById("16pm").style.background = "#B2BFC2";
-} else document.getElementById("16pm").style.background = "#CCDCF2";
+    document.getElementById("16pm").style.background = "#D0F9E5";
+} else document.getElementById("16pm").style.background = "#BBF0D6";
 
+//17-17pm (next day) assigns color depending on time of day: There are 3 avilable options.
 var today = new Date().getHours();
 if (today >= 17 && today <= 17.59) {
-    document.getElementById("17pm").style.background = "#5A7BB5";
+    document.getElementById("17pm").style.background = "#E2EEC2";
 } else if (today >= 17.59 && today <= 17) {
-    document.getElementById("17pm").style.background = "#B2BFC2";
-} else document.getElementById("17pm").style.background = "#CCDCF2";
+    document.getElementById("17pm").style.background = "#D0F9E5";
+} else document.getElementById("17pm").style.background = "#BBF0D6";
 
-//const bad = "null";
-//const good = bad.replace("null", " ");
 
-//let array = [0, 1, null, 2, 3];
 
-//function removeNull(array) {
-//return array.filter(x => x !== null)
-//};
+//Variable that has nothing, used so that instead of textbox displaying null it displays blank
+let activity = ""
 
-//function textarea1() {
+//9AM Text_____________________________________________________________________________________
 let textarea1 = localStorage.getItem("textArea1");
 document.getElementById("9am").innerHTML = "" + textarea1;
-//if (textarea1 = "null");{
-//textarea1.replace(" "); 
-//}
-//}
+//returns null
 
-let textarea2 = localStorage.getItem("textArea2");
-document.getElementById("10am").innerHTML = "" + textarea2;
-
-let textarea3 = localStorage.getItem("textArea3");
-document.getElementById("11am").innerHTML = "" + textarea3;
-
-let textarea4 = localStorage.getItem("textArea4");
-document.getElementById("12am").innerHTML = "" + textarea4;
-
-let textarea5 = localStorage.getItem("textArea5");
-document.getElementById("13pm").innerHTML = "" + textarea5;
-
-let textarea6 = localStorage.getItem("textArea6");
-document.getElementById("14pm").innerHTML = "" + textarea6;
-
-let textarea7 = localStorage.getItem("textArea7");
-document.getElementById("15pm").innerHTML = "" + textarea7;
-
-let textarea8 = localStorage.getItem("textArea8");
-document.getElementById("16pm").innerHTML = "" + textarea8;
-
-let textarea9 = localStorage.getItem("textArea9");
-document.getElementById("17pm").innerHTML = "" + textarea9;
-
-
-
-//if (textarea1 = "null");{
-    //textarea1.replace(" "); 
-    //}
-
-//const bad = "null";
-//const good = bad.replace("null", " ");
-
-
+ if( textarea1 === null){
+    localStorage.setItem("textArea1", activity);
+    document.getElementById("9am").innerHTML = "" + activity;
+} 
 
 document.getElementById("button9").onclick = function ()  {
     localStorage.setItem ("textArea1", document.getElementById("9am").value);
 }
 
+if (textarea1 != null){
+    document.getElementById("9am").innerHTML = "" + textarea1;
+}
+
+//10AM Text____________________________________________________________________________________
+let textarea2 = localStorage.getItem("textArea2");
+document.getElementById("10am").innerHTML = "" + textarea2;
+//returns null
+
+ if( textarea2 === null){
+    localStorage.setItem("textArea2", activity);
+    document.getElementById("10am").innerHTML = "" + activity;
+} 
+
 document.getElementById("button10").onclick = function ()  {
     localStorage.setItem ("textArea2", document.getElementById("10am").value);
 }
+
+if (textarea2 != null){
+    document.getElementById("10am").innerHTML = "" + textarea2;
+}
+
+//11AM Text____________________________________________________________________________________
+let textarea3 = localStorage.getItem("textArea3");
+document.getElementById("11am").innerHTML = "" + textarea3;
+//returns null
+
+ if( textarea3 === null){
+    localStorage.setItem("textArea3", activity);
+    document.getElementById("11am").innerHTML = "" + activity;
+} 
 
 document.getElementById("button11").onclick = function ()  {
     localStorage.setItem ("textArea3", document.getElementById("11am").value);
 }
 
+if (textarea3 != null){
+    document.getElementById("11am").innerHTML = "" + textarea3;
+}
+
+//12AM Text____________________________________________________________________________________
+let textarea4 = localStorage.getItem("textArea4");
+document.getElementById("12am").innerHTML = "" + textarea4;
+//returns null
+
+ if( textarea4 === null){
+    localStorage.setItem("textArea4", activity);
+    document.getElementById("12am").innerHTML = "" + activity;
+} 
+
 document.getElementById("button12").onclick = function ()  {
     localStorage.setItem ("textArea4", document.getElementById("12am").value);
 }
+
+if (textarea4 != null){
+    document.getElementById("12am").innerHTML = "" + textarea4;
+}
+
+//13PM Text____________________________________________________________________________________
+let textarea5 = localStorage.getItem("textArea5");
+document.getElementById("13pm").innerHTML = "" + textarea5;
+//returns null
+
+ if( textarea5 === null){
+    localStorage.setItem("textArea5", activity);
+    document.getElementById("13pm").innerHTML = "" + activity;
+} 
 
 document.getElementById("button13").onclick = function ()  {
     localStorage.setItem ("textArea5", document.getElementById("13pm").value);
 }
 
+if (textarea5 != null){
+    document.getElementById("13pm").innerHTML = "" + textarea5;
+}
+
+//14PM Text____________________________________________________________________________________
+let textarea6 = localStorage.getItem("textArea6");
+document.getElementById("14pm").innerHTML = "" + textarea6;
+//returns null
+
+ if( textarea6 === null){
+    localStorage.setItem("textArea6", activity);
+    document.getElementById("14pm").innerHTML = "" + activity;
+} 
+
 document.getElementById("button14").onclick = function ()  {
     localStorage.setItem ("textArea6", document.getElementById("14pm").value);
 }
+
+if (textarea6 != null){
+    document.getElementById("14pm").innerHTML = "" + textarea6;
+}
+
+//15PM Text____________________________________________________________________________________
+let textarea7 = localStorage.getItem("textArea7");
+document.getElementById("15pm").innerHTML = "" + textarea7;
+//returns null
+
+ if( textarea7 === null){
+    localStorage.setItem("textArea7", activity);
+    document.getElementById("15pm").innerHTML = "" + activity;
+} 
 
 document.getElementById("button15").onclick = function ()  {
     localStorage.setItem ("textArea7", document.getElementById("15pm").value);
 }
 
+if (textarea7 != null){
+    document.getElementById("15pm").innerHTML = "" + textarea7;
+}
+
+//16PM Text____________________________________________________________________________________
+let textarea8 = localStorage.getItem("textArea8");
+document.getElementById("16pm").innerHTML = "" + textarea8;
+//returns null
+
+ if( textarea8 === null){
+    localStorage.setItem("textArea8", activity);
+    document.getElementById("16pm").innerHTML = "" + activity;
+} 
+
 document.getElementById("button16").onclick = function ()  {
     localStorage.setItem ("textArea8", document.getElementById("16pm").value);
 }
 
+if (textarea8 != null){
+    document.getElementById("16pm").innerHTML = "" + textarea8;
+}
+
+//17PM Text____________________________________________________________________________________
+let textarea9 = localStorage.getItem("textArea9");
+document.getElementById("17pm").innerHTML = "" + textarea9;
+//returns null
+
+ if( textarea9 === null){
+    localStorage.setItem("textArea9", activity);
+    document.getElementById("17pm").innerHTML = "" + activity;
+} 
+
 document.getElementById("button17").onclick = function ()  {
     localStorage.setItem ("textArea9", document.getElementById("17pm").value);
 }
+
+if (textarea9 != null){
+    document.getElementById("17pm").innerHTML = "" + textarea9;
+}
+
+
+
--- README.md
+++ README.md
@@ -1,89 +1,24 @@
-# 05 Third-Party APIs: Work Day Scheduler
+My code consists of 3 documents
+-index.html
+-style.css
+-script.js
 
-## Your Task
+The html code is changed and edited using the javascript and css.
+I had to create a schedule from 9-5pm that saves the information that is added to it and saved by the button.
 
-Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
-
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
-
-## User Story
-
-```md
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
-
-## Acceptance Criteria
-
-```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
-
-The following animation demonstrates the application functionality:
-
-![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+index.html:
+Consists of 
+-header
+-each text area
+-footer (For cleaner visual of page)
+Every div contains a text area that will then be edited using javascript 
 
 
-## Grading Requirements
+style.css
+Is editing simple styling elements of the html page.
 
-This homework is graded based on the following criteria: 
-
-### Technical Acceptance Criteria: 40%
-
-* Satisfies all of the above acceptance criteria plus the following:
-
-  * Uses a date utility library to work with date and time
-
-### Deployment: 32%
-
-* Application deployed at live URL
-
-* Application loads with no errors
-
-* Application GitHub URL submitted
-
-* GitHub repo contains application code
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate
-
-* Application user interface style is clean and polished
-
-* Application resembles the mock-up functionality provided in the homework instructions
-
-### Repository Quality: 13%
-
-* Repository has a unique name
-
-* Repository follows best practices for file structure and naming conventions
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages
-
-* Repository contains quality README file with description, screenshot, and link to deployed application
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application
-
-* The URL of the GitHub repository, with a unique name and a README describing the project
-
-- - -
-© 2022 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+script.js
+-timer
+-code to change colors depending on what time it is
+-code to add local storage elements into each individual text area considering the fact that it usually returns null if there is nothing in local storage.
+Therefore i made it so that by default when you open the page it automatically adds a blank text into each text area so that each text area appears empty instead of displaying null.
